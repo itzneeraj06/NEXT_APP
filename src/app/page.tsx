fa6599d7d3e21245a2afa411255082ea255ce930
itzneeraj06 <neerajchouhan0604@gmail.com>
2025-07-14 01:47:24 +0530

    add contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import { Form } from "@/components/Form";
 import HeroSection from "@/components/HeroSection";
+import { WavyBackground } from "@/components/ui/wavy-background";
+import { WavyBackgroundDemo } from "@/components/WavyBackground";
+import { WorldMapDemo } from "@/components/WorldMapDemo";
 
 export default function Home() {
   return (
     <>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection />
+        <WavyBackgroundDemo />
+        <Form />
       </main>
     </>
   );
