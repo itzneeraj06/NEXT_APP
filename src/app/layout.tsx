a69025de4efaa0fcf77a4fd55d7ba55facafdca8
itzneeraj06 <neerajchouhan0604@gmail.com>
2025-07-18 00:16:50 +0530

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hi, I'm Neeraj - I Build the Web",
-  description: "Hi, I'm Neeraj — a passionate Full-Stack Developer skilled in the MERN stack. I specialize in building scalable, responsive web applications that solve real-world problems. From crafting sleek frontends to architecting powerful backends, I bring ideas to life through clean and efficient code.",
+  title: "Neeraj Chouhan | Full Stack Developer",
+  description: "Neeraj Chouhan — a passionate Full-Stack Developer skilled in the MERN stack. I specialize in building scalable, responsive web applications that solve real-world problems. From crafting sleek frontends to architecting powerful backends, I bring ideas to life through clean and efficient code.",
   keywords: [
     "Neeraj",
     "Neeraj Chouhan",
